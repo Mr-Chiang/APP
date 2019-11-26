afb14285abd59ca4a3b40197bdfe08d8a6785a13
scxss <shacoxss@yeah.net>
2019-11-26 15:09:14 +0800

    组件库

---
+++ components/uni-load-more/uni-load-more.vue
@@ -0,0 +1,38 @@
+<template>
+	<!-- 加载动画 -->
+	<view class="cu-load load-modal" v-if="loadStatus">
+		<!-- <view class="cuIcon-emojifill text-orange"></view> -->
+		<image src="/static/logo.png" mode="aspectFit"></image>
+		<view class="gray-text">{{loadText}}</view>
+	</view>
+</template>
+<script>
+	export default {
+		props:['status','text'],
+		data() {
+			return {
+				loadStatus:this.status,
+				loadText:'undefined' != this.text?this.text:'加载中...'
+			}
+			
+		},
+		methods: {
+	
+		},
+		onLoad() {
+
+		},
+		watch:{
+			'status':function(data){
+				this.loadStatus = data
+			},
+			'text':function(data){
+				this.loadText = data
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
